acc0c873fcacb550cbfffc6fb705507611bd483f
lovish197 <lovisharora9253@gmail.com>
2021-02-22 12:07:17 +0530

    Room is working fine players are joing and scene is loading

--- Assets/PhotonScripts/NetworkControllerDiffApproach.cs
+++ Assets/PhotonScripts/NetworkControllerDiffApproach.cs
@@ -5,73 +5,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class NetworkControllerDiffApproach : MonoBehaviourPunCallbacks
 {
-    bool IsConnecting;
+    public Text Status_text;
     public TMP_InputField RoomName;
     const int maxPlayersPerRoom = 2;
-    public Button CreateRoom, _JoinRoom;
-    private void Awake()
-    {
-        PhotonNetwork.AutomaticallySyncScene = true;
-    }
+    public Button StartGame;
+
     // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
+        StartGame.transform.gameObject.SetActive(false);
+        Status("Connecting to the server");
     }
-    public void CreateTheRoom(string RoomName)
+
+    private void Status(string msg)
     {
-        IsConnecting = true;
-        PhotonNetwork.CreateRoom(RoomName, new RoomOptions { MaxPlayers = maxPlayersPerRoom }); // tis will create a room for us witha specific name
-                                                                                                // if wants a room with random name just applu null at the place of room name
+        Debug.Log(msg);
+        Status_text.text = msg;
     }
-    /*public void JoinRoom(string RoomName)
-    {
-        Debug.Log("Room manually create hua hai");
-        PhotonNetwork.JoinRoom(RoomName);
-    }*/
+
     public override void OnConnectedToMaster()
     {
-        CreateRoom.onClick.AddListener(() => { CreateTheRoom(RoomName.text); }); // create the room when button is pressed
-        //_JoinRoom.onClick.AddListener(() => { JoinRoom(RoomName.text); });
+        base.OnConnectedToMaster();
+        PhotonNetwork.AutomaticallySyncScene = true;
+        StartGame.transform.gameObject.SetActive(true);
+
+        Status("Connected to " + PhotonNetwork.ServerAddress);
     }
     public override void OnDisconnected(DisconnectCause cause)
     {
         Debug.Log($"Disconnected due to : {cause} ");
     }
-
-    /*public override void OnJoinRandomFailed(short returnCode, string message) // this method is for random rooms will modify later for specific room
+    public void On_start_Click()
     {
-        Debug.Log("no room available, hence creating a new room");
-        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
-    }*/
+        string roomname = RoomName.text;
+        Debug.Log(roomname);
 
+        RoomOptions options = new RoomOptions();
+        options.IsOpen = true;
+        options.IsVisible = true;
+        options.MaxPlayers = maxPlayersPerRoom;
+
+        PhotonNetwork.JoinOrCreateRoom(roomname, options, TypedLobby.Default);
+        StartGame.transform.gameObject.SetActive(false);
+        Status("Joining " + roomname);
+    }
     public override void OnJoinRoomFailed(short returnCode, string message)
     {
-        Debug.Log("no room available, hence creating a new room");
-        CreateTheRoom(RoomName.text);
+        Status("no room available");
     }
 
     public override void OnJoinedRoom()
     {
-        Debug.Log(" joined in " + PhotonNetwork.CurrentRoom.Name + " Room");
         int playerCount;
         playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
         if(playerCount != maxPlayersPerRoom)
         {
-            Debug.Log("less no of players inside the romm so waiting for other players to join");
+            Status("less no of players inside the romm so waiting for other players to join");
         }
         else
         {
-            Debug.Log(" all opponenets found, match is ready  to begin");
+            Status("All opponents found game is starting");
         }
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
-        if(PhotonNetwork.CurrentRoom.PlayerCount == maxPlayersPerRoom)
+        if(PhotonNetwork.CurrentRoom.PlayerCount == maxPlayersPerRoom) 
         {
             PhotonNetwork.CurrentRoom.IsOpen = false; // this avoids new player joining the room when the room is full
 
